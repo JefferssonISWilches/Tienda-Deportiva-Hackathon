8672e2367c2ee06f20cfce09642c24dc9ce0a157
otraVezLucho <otrodeluisvillada@gmail.com>
2025-11-24 12:35:25 -0500

    se agrega script que crea las tarjetas de los productos

---
+++ scripts/script.js
@@ -0,0 +1,92 @@
+
+
+document.body.style.margin = "0";
+
+const seccion_productos = document.getElementById("tarjeta-productos-js");
+seccion_productos.style.display = "flex";
+seccion_productos.style.justifyContent = "center";
+seccion_productos.style.padding = "10px";
+seccion_productos.style.width = "100%";
+seccion_productos.style.height = "500px";
+seccion_productos.style.backgroundColor = "black";
+seccion_productos.style.border = "2px solid black";
+
+seccion_productos.append(container_tarjetas());
+
+
+
+//se crea Container div que esta dentro de la seccion tarjeta-productos-js para mostrar las tarjetas de productos: 
+
+function container_tarjetas(){
+    const container_tarjetas = document.createElement("div");
+    container_tarjetas.classList.add("container-tarjetas");
+    container_tarjetas.style.width = "90%";
+    container_tarjetas.style.height = "300px"
+    container_tarjetas.style.border = "2px solid #e70000ff";
+    container_tarjetas.style.display = "flex";
+    container_tarjetas.style.padding = "15px";
+    container_tarjetas.appendChild(tarjeta_productos("juguete","carrito de color rojo",20));
+    return container_tarjetas;
+
+}
+
+//Se crea la tarjeta de productos y 
+function tarjeta_productos(nombre,descripcion, precio, img) {
+
+    //container de la tarjeta
+    const tarjeta_productos = document.createElement("div");
+    tarjeta_productos.classList.add("tarjeta-producto");
+    tarjeta_productos.style.display = "flex";
+    tarjeta_productos.style.flexFlow = "column";
+    tarjeta_productos.style.justifyContent = "top";
+    tarjeta_productos.style.width = "20%";
+    tarjeta_productos.style.border = "2px solid #e70000ff";
+
+    //Imagen
+    const imagen_producto = document.createElement("img");
+    imagen_producto.classList.add("imagen-producto");
+    imagen_producto.src = img;
+    imagen_producto.style.width = "100%";
+    imagen_producto.style.height = "50%";
+    imagen_producto.style.backgroundColor = "#f0f0f0"
+    
+    //nombre
+    const nombre_producto = document.createElement("h3");
+    nombre_producto.classList.add("nombre-articulo");
+    nombre_producto.innerText = nombre;
+    nombre_producto.style.color = "#ffffff";
+    nombre_producto.style.textAlign = "start";
+    nombre_producto.style.marginTop = "5px";
+    nombre_producto.style.marginBottom = "5px";
+
+    //Descripcion 
+
+    const descripcion_producto = document.createElement("p");
+    descripcion_producto.classList.add("descripcion-producto");
+    descripcion_producto.innerText = descripcion;
+    descripcion_producto.style.color = "#ffffff";
+    descripcion_producto.style.marginTop = "5px";
+    descripcion_producto.style.marginBottom = "5px";
+
+    //precio
+
+    const precio_producto = document.createElement("p");
+    precio_producto.classList.add("precio-preducto");
+    precio_producto.innerText = 'Valor $' + precio;
+    precio_producto.style.color = "#ffffff";
+    precio_producto.style.marginTop = "5px";
+    precio_producto.style.marginBottom = "5px";
+
+    //boton
+    const boton_carrito = document.createElement("button");
+    boton_carrito.classList.add("agregar-carrito");
+    boton_carrito.type= "button";
+    boton_carrito.innerText = "🛒";
+    boton_carrito.style.width = "40px"
+
+    
+    //agregar todos los componentes a la tarjeta
+    tarjeta_productos.append(imagen_producto, nombre_producto, descripcion_producto, precio_producto, boton_carrito);
+    
+    return tarjeta_productos;
+}   
